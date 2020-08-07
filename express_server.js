b7fa145a066c8983b9d8740bed4b9eb3043dfd33
Serah Sirohi <serahsirohi@gmail.com>
2020-08-07 23:54:30 +0000

    after storing passwords securely exercise

--- express_server.js
+++ express_server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const app = express();
-app.use(cookieParser());
-
+const bcrypt = require('bcrypt');
 const PORT = 8080; //default port 8080
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 const users = {
   "userRandomID": {
@@ -19,8 +23,6 @@ const users = {
 }
 
 function getUsers(user_id) {
-  // console.log("user_id:",user_id);
-  // console.log("users:", users); 
   return users[user_id];
 }
 
@@ -32,14 +34,14 @@ function getUserByEmail(email) {
     } else {
       return {};
     }
-  } 
+  }
 }
 
 function checkEmail(email) {
   for (key in users) {
     if (email === users[key].email) {
       return key;
-    } 
+    }
   }
 }
 
@@ -49,25 +51,20 @@ function generateRandomString() {
 
 function urlsForUser(id) {
   const filteredUrl = {};
-  
+
   for (let shortUrl in urlDatabase) {
     if (urlDatabase[shortUrl].userID === id) {
-      filteredUrl[shortUrl]=urlDatabase[shortUrl];
+      filteredUrl[shortUrl] = urlDatabase[shortUrl];
     }
   }
   return filteredUrl;
 }
 
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "user2RandomID" },
-  "9sm5xk": { longURL: "http://www.google.com", userID: "userRandomID"}
+  "9sm5xk": { longURL: "http://www.google.com", userID: "userRandomID" }
 };
 
 app.get("/", (req, res) => {
@@ -83,13 +80,10 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  // console.log(req.cookies);
   const user = getUsers(req.cookies["user_id"]);
   console.log('user:', user);
-  // console.log('templateVars:',templateVars);
-  
+
   if (user) {
-    // res.render("urls", templateVars);
     const urls = urlsForUser(user.id);
     const templateVars = { urls: urls, user: user };
     res.render("urls_index", templateVars);
@@ -98,46 +92,43 @@ app.get("/urls", (req, res) => {
   }
 });
 
-
 app.get("/urls/new", (req, res) => {
   const user = getUsers(req.cookies["user_id"]);
   const templateVars = { user: user };
   if (user) {
     res.render("urls_new", templateVars);
   } else {
-    // res.redirect("/login");
     res.status(401).send("Unauthorized. Please login first before trying to access this page.");
   }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  console.log("shorturl:",shortURL);
+  console.log("shorturl:", shortURL);
   const user = getUsers(req.cookies["user_id"]);
-  const templateVars = { user:user, shortURL:shortURL, longURL:urlDatabase[shortURL].longURL };
-  console.log("templateVars:",templateVars);
-  // const user = req.cookies["user_id"];
+  const templateVars = { user: user, shortURL: shortURL, longURL: urlDatabase[shortURL].longURL };
+  console.log("templateVars:", templateVars);
 
   if (user) {
     res.render("urls_show", templateVars);
   } else {
     res.status(401).send("Unauthorized. Please login first before trying to access this page.");
   }
-  
+
 });
 
 app.post("/urls/new", (req, res) => {
   const key = generateRandomString();
-  urlDatabase[key] = { longURL: req.body.longURL, userID: req.cookies["user_id"]}; 
-  console.log('urlDatabase:',urlDatabase);
+  urlDatabase[key] = { longURL: req.body.longURL, userID: req.cookies["user_id"] };
+  console.log('urlDatabase:', urlDatabase);
   res.redirect(`/urls/${key}`);
-      
+
 });
 
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL]["longURL"];
- 
+
   if (!longURL) {
     res.status(404).send();
   } else {
@@ -147,26 +138,26 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   console.log(req.body);
-  
+
   const userid = req.cookies["user_id"];
   const shortURL = req.params.shortURL;
   const obj = urlDatabase[shortURL];
 
   if (obj !== userid) {
     res.status(401).send("Unauthorized. Please login first before trying to access this page.");
-    return; 
-  } 
+    return;
+  }
 
   delete urlDatabase[shortURL];
   res.redirect("/urls");
-  
+
 });
 
 app.post("/urls/:shortURL", (req, res) => {
   console.log(req.body);
   const user = req.cookies["user_id"];
   const shortURL = req.params.shortURL;
-  const templateVars = { user:user, shortURL:shortURL, longURL:urlDatabase[shortURL].longURL };
+  const templateVars = { user: user, shortURL: shortURL, longURL: urlDatabase[shortURL].longURL };
 
   if (user) {
     const longUrl = urlDatabase[shortURL].longURL;
@@ -177,7 +168,6 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  // console.log(req.body);
   res.clearCookie("user_id");
   res.redirect("/login");
 });
@@ -190,13 +180,15 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const user_id = generateRandomString();
   const { email, password } = req.body;
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
   if (!email || !password) {
-    // console.log('user_id:',user_id);
     res.status(400).send("Registration failed. Email and/or Password fields cannot be empty.");
   } else if (checkEmail(email)) {
     res.status(400).send("Registration failed. Email already exists. Please login.");
   } else {
-    users[user_id] = { id: user_id, email, password };
+    users[user_id] = { id: user_id, email, password: hashedPassword };
     res.cookie('user_id', user_id);
     res.redirect("/urls");
   }
@@ -204,18 +196,17 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  // console.log(req.cookies.user_id);   
   console.log(req.body.email);
   console.log(req.body.password);
 
   const email = req.body.email;
   const password = req.body.password;
   const userExists = checkEmail(email);
-  // console.log(userinfo);
 
-      if (!userExists) {
+  if (!userExists) {
     res.status(403).send("Forbidden.")
-  } else if (users[userExists].password !== password) {
+  } else if (!bcrypt.compareSync(password, users[userExists].password)) {
+    console.log("Here's my pwd:", users[userExists].password)
     res.status(403).send("Incorrect Password.");
   } else {
     res.cookie('user_id', userExists);
